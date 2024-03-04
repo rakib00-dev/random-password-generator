221f55c4af7401a9eb0d54b6296289546f5aff4c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-03-04 19:42:19 +0600

    added the last functionality in the generator

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const button = document.getElementById('btn');
 function lengthShow() {
   lengthValShow.innerHTML = lengthInput.value;
 }
-
 setInterval(lengthShow, 10);
 
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
@@ -14,8 +13,8 @@ const upperCase = lowerCase.toUpperCase();
 const number = '0123456789';
 const symble = '!@#$%^&*()_+-=[]{};\':"\\|,.<>/?';
 
-const upperCaseInput = document.getElementById('uppercase');
-const lowerCaseInput = document.getElementById('lowercase');
+const upperCaseInput = document.getElementById('upperCase');
+const lowerCaseInput = document.getElementById('lowerCase');
 const numberInput = document.getElementById('number');
 const symbleInput = document.getElementById('symble');
 
@@ -24,13 +23,38 @@ const getRandomData = (dataSet) => {
 };
 
 const passwordGenerate = (password = '') => {
-  if (upperCaseInput.clicked) {
+  if (upperCaseInput.checked) {
     password += getRandomData(upperCase);
   }
+  if (lowerCaseInput.checked) {
+    password += getRandomData(lowerCase);
+  }
+  if (numberInput.checked) {
+    password += getRandomData(number);
+  }
+  if (symbleInput.checked) {
+    password += getRandomData(symble);
+  }
 
+  if (password.length < lengthInput.value) {
+    return passwordGenerate(password);
+  }
+
+  document.getElementById('password').value = truncateString(
+    password,
+    lengthInput.value
+  );
   console.log(password);
 };
 
-button.addEventListener('click', function () {
-  generatePassword();
+button.addEventListener('click', () => {
+  passwordGenerate();
 });
+
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num);
+  } else {
+    return str;
+  }
+}
